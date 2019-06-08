1faa273e9b91f8ebd17fd2eb53afbaa677fab3d7
Tharazi97 <44876530+Tharazi97@users.noreply.github.com>
2019-06-08 17:56:54 +0200

    tak jakby miasto dodane

--- Poczta2/Poczta2/Miasto.cs
+++ Poczta2/Poczta2/Miasto.cs
@@ -9,6 +9,41 @@ namespace Poczta2
 {
     class Miasto
     {
-        public Queue<Klient> klienci;
+        public char nazwa;
+        public Queue<Klient> klienci = new Queue<Klient>();
+        public List<Dostawczak> dostawczakiDoRozladunku = new List<Dostawczak>();
+        public List<Dostawczak> dostawczakiDoZaladunku = new List<Dostawczak>();
+        public Skrzynka[] skrzynki;
+        public Okienko[] okienka;
+        public Mutex mutSorto = new Mutex();
+        public List<Pracownik> pracownicy = new List<Pracownik>();
+        public List<Thread> threads = new List<Thread>();
+        public Kierowniczka kierowniczka;
+        public Thread kier;
+
+        public static List<Miasto> miasta = new List<Miasto>();
+
+        Random rnd = new Random();
+        DateTime time = new DateTime();
+        bool dziewiata = false;
+
+        public Miasto(char nazwa, int iloscOkienek, int iloscMiast)
+        {
+            this.nazwa = nazwa;
+
+            skrzynki = new Skrzynka[iloscMiast];
+            for(int i=0; i<iloscMiast; i++)
+            {
+                skrzynki[i] = new Skrzynka();
+                skrzynki[i].miasto = (char)(i + 65);
+            }
+
+            okienka = new Okienko[iloscOkienek];
+            for(int i=0; i<iloscOkienek; i++)
+            {
+                okienka[i] = new Okienko();
+            }
+        }
+
     }
 }
